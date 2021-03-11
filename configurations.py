f6ebae3626361e5aea25b7769981997d36c8a4dc
Befrimon Reminodal <superaijen@gmail.com>
2021-03-11 23:21:53 +0300

    Add animation.

--- configurations.py
+++ configurations.py
@@ -13,9 +13,12 @@ HEIGHT = GetSystemMetrics(1)
 
 
 class Settings:
-    with open("settings.json", encoding="utf8") as sett:
+    with open("files/settings.json", encoding="utf8") as sett:
         a11 = json.loads(sett.read())
     mode = a11["mode"]
+    background = pygame.image.load(a11["bg_dir"])
+
+
 settings = Settings
 pygame.init()
 screen = pygame.display.set_mode((270, 470))
@@ -32,38 +35,53 @@ images = Images
 
 class Sprites:
     class Button(pygame.sprite.Sprite):
-        def __init__(self, coord_x, coord_y, image):
+        def __init__(self, coord_x, coord_y, w, h, image, f):
             pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.transform.scale(image, (int((WIDTH * 48 // 100)), int(HEIGHT * 17.5 // 100)))
+            self.image = pygame.transform.scale(image, (w, h))
             self.image.set_colorkey((255, 255, 255))
             self.rect = self.image.get_rect()
             self.rect.x = coord_x
             self.rect.y = coord_y
+            self.width = w
+            self.height = h
+            self.frame = 1
+            self.f = f
+
+        def mouse_in(self):
+            img = pygame.image.load(os.path.join(f'images/buttons/start/{str(self.frame)}.png')).convert()
+            self.image = img
+            self.frame += 1
+            if self.frame > self.f:
+                self.frame -= 1
 
     class Scientist(pygame.sprite.Sprite):
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.frame = 24
             self.image = pygame.image.load(
-                os.path.join(f'images/scientist/{str(self.frame)}.png')).convert()
+                os.path.join(f'images/scientist/0.png')).convert()
             self.image.set_colorkey(WHITE)
             self.rect = self.image.get_rect()
             self.position = [(WIDTH * 0.35) // 2, HEIGHT - ((HEIGHT * 0.4) // 2)]
             self.rect.center = (self.position[0], self.position[1])
 
         def walk(self, direction):
+            m = 0
             if direction == "LEFT":
-                img = pygame.image.load(os.path.join(f'images/scientist/{str(self.frame)}.png')).convert()
+                img = pygame.image.load(os.path.join(f'images/scientist/walk/{str(self.frame)}.png')).convert()
+                m = -5
             elif direction == "RIGHT":
-                img = pygame.image.load(os.path.join(f'images/scientist/{str(self.frame+24)}.png')).convert()
+                img = pygame.image.load(os.path.join(f'images/scientist/walk/{str(self.frame+24)}.png')).convert()
+                m = 5
             else:
                 img = None
-            self.image = pygame.transform.scale(img, (int((WIDTH * 0.282)), int(HEIGHT * 0.3786)))
+            # self.image = pygame.transform.scale(img, (int((WIDTH * 0.282)), int(HEIGHT * 0.3786)))
+            self.image = img
             print(int((WIDTH * 0.282)), int(HEIGHT * 0.3786))
             self.image.set_colorkey(WHITE)
-            self.position[0] += 5
+            self.position[0] += m
             self.frame += 1
-            if self.frame > 24:
+            if self.frame > 23:
                 self.frame = 1
             self.rect.center = (self.position[0], self.position[1])
 
@@ -91,7 +109,9 @@ class Sprites:
 
     menu_elements = pygame.sprite.Group()
     bottle = Bottle()
-    start = Button(int(WIDTH * 0.347), int(HEIGHT * 0.26), images.start_btn)
+    start = Button(int(WIDTH * 0.347), int(HEIGHT * 0.26),
+                   int((WIDTH * 48 // 100)), int(HEIGHT * 17.5 // 100),
+                   images.start_btn, 5)
     menu_elements.add(bottle, start)
 
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
+import os
 import configurations as config
-
-FPS = 50
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
@@ -17,28 +16,47 @@ game = False
 
 while running:
     while menu:
-        clock.tick(FPS)
+        clock.tick(30)
         screen.blit(config.backgrounds.menu, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 menu = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                position = event.pos
+                if config.sprites.start.rect.x < position[0] < config.sprites.start.rect.x + config.sprites.start.width:
+                    if config.sprites.start.rect.y < position[1] < config.sprites.start.rect.y + config.sprites.start.height:
+                        menu = False
+                        game = True
+            if event.type == pygame.MOUSEMOTION:
+                position = event.pos
+                if config.sprites.start.rect.x < position[0] < config.sprites.start.rect.x + config.sprites.start.width:
+                    if config.sprites.start.rect.y < position[1] < config.sprites.start.rect.y + config.sprites.start.height:
+                        config.sprites.start.mouse_in()
+                    else:
+                        config.sprites.start.image = pygame.image.load(os.path.join(f'images/buttons/start/1.png')).convert()
+        config.sprites.menu_elements.draw(screen)
+        config.sprites.bottle.update()
+
+        pygame.display.flip()
+
+    while game:
+        clock.tick(50)
+        # screen.blit(config.backgrounds.menu, (0, 0))
+        screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                game = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
                     config.sprites.scientist.walk("RIGHT")
                 if event.key == pygame.K_a:
                     config.sprites.scientist.walk("LEFT")
-        config.sprites.menu_elements.draw(screen)
+            if event.type == pygame.KEYUP:
+                config.sprites.scientist.image = pygame.image.load(os.path.join(f'images/scientist/0.png')).convert()
+        config.sprites.player.draw(screen)
 
         pygame.display.flip()
-
-    while game:
-        clock.tick(FPS)
-        screen.blit(config.backgrounds.menu, (0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                menu = False
-        config.sprites.player.draw(screen)
 
 pygame.quit()
